d256034679bcdec36cc581a08c3322d66c27917e
jimmy hu <60071jimmy@gmail.com>
2018-02-27 23:36:35 +0800

    Add include header
    Add include header

--- CameraImageProcessing/MFCaptureD3D/BasicFileIO/BasicFileIO.h
+++ CameraImageProcessing/MFCaptureD3D/BasicFileIO/BasicFileIO.h
@@ -12,6 +12,7 @@
 #include <stdlib.h>														//	引入標頭檔stdlib.h
 #include <string.h>														//	引入標頭檔string.h
 #include "..\\unistd.h"													//	引入標頭檔unistd.h
+#include "..\\ImageProcessing\\DIPDefine.h"
 //-----副程式宣告區----- 
 /*	FileExistCheck副程式用於
 	在讀寫檔案前，有時需先確認檔案是否已存在，在此設計FileExistCheck(檔案存在檢查)副程式執行檔案存在與否之檢查工作。
